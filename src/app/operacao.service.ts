e4c8039d18f03171f220fedf7c3af14d82bcfc52
Técnico em Informática 2019.1 <42121742019.1@senacrjedu.local>
2021-12-20 18:51:53 -0300

    Terceiro

--- src/app/operacao.service.ts
+++ src/app/operacao.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OperacaoService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  extrato(){
+    return this.firestore.collection("operacao").snapshotChanges();
+  }
 }
